93647a1edd747191c238b22d2951b456541638d0
OWMAS <owen.mason@nhs.net>
2022-12-16 09:39:14 +0000

    Tweak 50k Check and Implement 50k Check Correction

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,15 @@
 // ES6 or Vanilla JavaScript
+
+let back = false;
+
+function swapSides() {
+    if (back) {
+        document.getElementById("prescription-image").src = "/images/E000001NE02_side1.jpg";
+        document.getElementById("swap-sides").innerHTML = "Show Back";
+    } else {
+        document.getElementById("prescription-image").src = "/images/E000001NE02_side2.jpg";
+        document.getElementById("swap-sides").innerHTML = "Show Front";
+    }
+
+    back = !back;
+}
